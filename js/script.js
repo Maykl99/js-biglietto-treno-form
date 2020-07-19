f3cc28d1e905dddb71f9ab992cf0ba3014657d69
Maykl Hakobyan <maicolhakobyan853@yahoo.it>
2020-07-19 22:16:41 +0200

    app js

--- js/script.js
+++ js/script.js
@@ -1,24 +1,51 @@
 /* Partendo dall'esercizio della scorsa settimana aggiungiamo la parte visiva come da screenshot
 Creiamo un finto biglietto del treno con:
 Nome passeggero 
-Codice treno (numero casuale tra 90000 e 100000 escluso) fatto
-Numero carrozza fatto
-Prezzo calcolato fatto
-Categoria selezionata dall'utente
-Aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.
-Buon lavoro e buon viaggio! */
-
-
-
-
-
+Codice treno (numero casuale tra 90000 e 100000 escluso) 
+Numero carrozza 
+Prezzo calcolato 
+Categoria selezionata dall'utente; aggiungiamo una piccola animazione al click su "Crea" e "Annulla", se clicchiamo su annulla dobbiamo ripulire il form.*/
 
 var genera=document.getElementById("genera").addEventListener("click", funzioneGenera);
 var annulla=document.getElementById("annulla").addEventListener("click",funzioneAnnulla);
 
+// funzione genera al click del pulsante genera devo prendere i valori passati dagli input, nome e cognome, distanza di km e la fascia d'età;
+function funzioneGenera() {
+
+    var mostra=document.getElementById('utente').className='show';
+
+    // genera il prezzo del biglietto in base alla distanza
+    var utentePrezzo= document.getElementById('prelevaDistanza').value;
+    var prezzo=costoBiglietto(utentePrezzo);
+    // eta //-> offerta
+    var fasciaEta=document.getElementById('eta').value;
+    // sconto in base all'età dell'utente
+    var messaggio='Tariffa standard';
+
+    if(fasciaEta == 'minorenne'){
+      prezzo = utentePrezzo * 20 / 100;
+      messaggio='Tariffa minorenni';
+    }
+    else if(fasciaEta == 'over65'){
+      prezzo = utentePrezzo * 10 / 100;
+      messaggio = 'Tariffa Silver';
+    }
+
+    fasciaEta = document.getElementById('offerta').innerHTML=messaggio;
+    utentePrezzo += document.getElementById('prezzo').innerHTML= prezzo + ' €'; //costoBiglietto(utentePrezzo);
+
+    // genera codici randomici di carrozza e codice cp
+    var codiceCp=document.getElementById('codiceCp').innerHTML=getRandomInt(1,11);
+    var carrozza=document.getElementById('carrozza').innerHTML=getRandomInt(90000,100000);
+    
+    // genera il nome del passeggero
+    var utente= document.getElementById('preleva').value;
+    utente += document.getElementById('nomePasseggero').innerHTML=utente; 
+}
 
 function funzioneAnnulla(){
-
+    var nascondi=document.getElementById('utente').className='hidden';
+ 
     // annulla codici randomici di carrozza e codice cp
     var codiceCp=document.getElementById('codiceCp').innerHTML='';
     var carrozza=document.getElementById('carrozza').innerHTML='';
@@ -31,64 +58,25 @@ function funzioneAnnulla(){
     var utentePrezzo= document.getElementById('prelevaDistanza').value;
     utentePrezzo += document.getElementById('prezzo').innerHTML=''; 
 
-  return codiceCp, carrozza, utente, utentePrezzo; //etaPrezzoUtente;
-}
+} 
 
 
 
-
-// funzione genera al click del pulsante genera devo prendere i valori passati dagli input, nome e cognome, distanza di km e la fascia d'età;
-
-function funzioneGenera() {
-
-    // genera codici randomici di carrozza e codice cp
-    var codiceCp=document.getElementById('codiceCp').innerHTML=getRandomInt();
-    var carrozza=document.getElementById('carrozza').innerHTML=numeroCarrozza();
-    
-    // genera il nome del passeggero
-    var utente= document.getElementById('preleva').value;
-    utente += document.getElementById('nomePasseggero').innerHTML=utente;
-
-    // genera il prezzo del biglietto in base alla distanza
-    var utentePrezzo= document.getElementById('prelevaDistanza').value;
-    utentePrezzo += document.getElementById('prezzo').innerHTML=costoBiglietto(utentePrezzo); 
-
-
-    fasciaEta = document.getElementById('offerta').innerHTML=fasciaEta;
-
-  return codiceCp, carrozza, utente, utentePrezzo, fasciaEta; //etaPrezzoUtente;
-}
-
-
-
-// funzione che ritorna il prezzo in base alla distanza dei km da percorrere e in base all'età dell'utente
-function costoBiglietto(kmDaPercorrere='',eta=''){
+// funzione che ritorna il prezzo in base alla distanza dei km da percorrere
+function costoBiglietto(kmDaPercorrere){
     var prezzoKm=0.21;
     prezzoKm *= kmDaPercorrere;
-    if(eta < 18){
-        prezzoKm * 20 / 100;
-    }else if(eta >= 65){
-        prezzoKm * 40 / 100;
-    }else{
-        prezzoKm;
-    }
-
-    return prezzoKm + ' €';
+    return prezzoKm;
 }
 
 
+//funzione genera un numero Carrozza randomico fra 1 e 10
 // funzione che genera un codice treno casuale con un intervallo fra 90.000 e 100.000 escluso.
-function getRandomInt(){
-    var min = Math.ceil(90000);
-    var max = Math.floor(100000);
+function getRandomInt(min,max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
     var esito=Math.floor(Math.random() * (max - min)) + min;
     return esito; //Il max è escluso e il min è incluso
 }
 
-//funzione genra un numero Carrozza randomico fra 1 e 10 incluso 
-function numeroCarrozza(){
-    var random=null;
-    random= Math.floor(Math.random()*10)+1;
-    return random;
-}
 
